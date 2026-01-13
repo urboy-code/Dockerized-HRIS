59adc8aa4369695f485ec447b4cefd4ad4e9e3b2
urboy-code <42699288+urboy-code@users.noreply.github.com>
2026-01-13 09:47:40 +0700

    feat: Authentication with JWT Token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,23 +1,57 @@
-const prisma = require('../config/db');
+const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const login = async (req, res) => {
+const prisma = new PrismaClient();
+const { JWT_SECRET } = process.env;
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email has been used ❌' });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+        role: role || 'STAFF',
+      },
+    });
+
+    res.status(201).json({
+      message: 'User successfuly created ✅',
+      data: user,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Failed to register user' });
+  }
+};
+
+exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const user = await prisma.user.findUnique({
-      where: { email },
-    });
-
+    const user = await prisma.user.findUnique({ where: { email } });
     if (!user) {
-      return res.status(400).json({ message: 'Wrong Email or Password!' });
+      return res.status(404).json({
+        message: 'User not found ❌',
+      });
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Wrong Email or Password!' });
+    if (!isMatch) {
+      return res.status(401).json({
+        message: 'Wrong Password!',
+      });
     }
 
     const token = jwt.sign(
@@ -25,25 +59,22 @@ const login = async (req, res) => {
         id: user.id,
         role: user.role,
       },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: '1d',
-      }
+      JWT_SECRET,
+      { expiresIn: '1d' }
     );
 
     res.json({
-      message: 'Login Success!',
-      token,
-      user: {
+      message: 'Login success ✅',
+      data: {
         id: user.id,
         name: user.name,
+        email: user.email,
         role: user.role,
+        token,
       },
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: 'Failed to login user' });
   }
 };
-
-module.exports = { login };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { login } = require('../controllers/auth.controller');
+const { login, register } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
+router.post('/register', register);
 router.post('/login', login);
 
 module.exports = router;
